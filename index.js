fd2fc8910b7dc9b312948557e28120e36f63d821
twismdx <twism@live.com>
2023-02-13 16:08:01 +1030

    a

--- index.js
+++ index.js
@@ -7,12 +7,27 @@ app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `)
 })
 
-app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳')
+app.get('/livestream', async function (req, res) {
+	const { param } = req.query
+	const multi =
+		'https://www.poolstat.net.au/livestream/multimatch?key=TUroCx2IxK7p8fZLIK6cS6O~9xritQk~&api=1&drid='
+	const single =
+		'https://www.poolstat.net.au/livestream/multimatch?key=TUroCx2IxK7p8fZLIK6cS6O~9xritQk~&api=1&matchid='
+	if (`${param}` === drid) {
+		res.status(200).send({ MultiMatch: multi })
+	} else if (param === matchid) {
+		res.status(200).send({ SingleMatch: single })
+	} else {
+		res.send('Invalid request')
+	}
 })
 
-app.get('/about', (req, res) => {
-  res.send('This is my about route..... ')
+app.post('/matchid', async function (req, res) {
+	const { matchid, drid } = req.body
+	//do something
+	res.send(
+		`https://www.poolstat.net.au/livestream/multimatch?key=TUroCx2IxK7p8fZLIK6cS6O~9xritQk~&api=1&drid=${matchid}`
+	)
 })
 
 // Export the Express API
