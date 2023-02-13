0577389d7559b4e65b79c55cf3e169fc60ef1f7e
twismdx <twism@live.com>
2023-02-13 15:58:04 +1030

    a

--- api/app.js
+++ api/app.js
@@ -1,14 +1,30 @@
-//functionality of a route
+'use strict'
+require('dotenv').config()
 
-const livestream = require('express').Router()
+const express = require('express')
+const cors = require('cors')
+const helmet = require('helmet')
 
-livestream.get('/livestream', async function (req, res) {
+var allowedOrigins = ['https://example.com']
+/* var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Operation not allowed'))
+    }
+  }
+} */
+
+const app = express()
+
+app.get('/livestream', async function (req, res) {
 	const { param } = req.query
 	const multi =
 		'https://www.poolstat.net.au/livestream/multimatch?key=TUroCx2IxK7p8fZLIK6cS6O~9xritQk~&api=1&drid='
 	const single =
 		'https://www.poolstat.net.au/livestream/multimatch?key=TUroCx2IxK7p8fZLIK6cS6O~9xritQk~&api=1&matchid='
-	if (param === drid) {
+	if (`${param}` === drid) {
 		res.status(200).send({ MultiMatch: multi })
 	} else if (param === matchid) {
 		res.status(200).send({ SingleMatch: single })
@@ -17,7 +33,7 @@ livestream.get('/livestream', async function (req, res) {
 	}
 })
 
-livestream.post('/matchid', async function (req, res) {
+app.post('/matchid', async function (req, res) {
 	const { matchid, drid } = req.body
 	//do something
 	res.send(
@@ -25,4 +41,7 @@ livestream.post('/matchid', async function (req, res) {
 	)
 })
 
-module.exports = livestream
+app.use(helmet())
+/* app.use(cors(corsOptions)); */
+
+module.exports = app
